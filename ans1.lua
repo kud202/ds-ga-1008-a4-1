7025ec23e91a87a1d1085b738a101e1b56629760
kud202 <karandesai@nyu.edu>
2016-05-03 12:23:12 -0400

    Added forward and backward pass code.

--- ans1.lua
+++ ans1.lua
@@ -21,3 +21,10 @@ a = nn.CAddTable()({
   }),
   z
 })
+g = nn.gModule({x,y,z},{a})
+x_in = torch.rand(1,4)
+y_in = torch.rand(1,5)
+z_in = torch.rand(1,2)
+grad = torch.Tensor({1,1})
+f = g:forward({x_in,y_in,z_in})
+b = g:backward({x_in,y_in,z_in},grad)
